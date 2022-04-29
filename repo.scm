375a1fe66a5916a3ec36f4e38287eca968fead4c
Howard Winslow <395849+hwinslow3@users.noreply.github.com>
2022-04-29 00:11:34 -0400

    added dummy sdl package

--- repo.scm
+++ repo.scm
@@ -1,4 +1,16 @@
 (repository
  (sibling
   (name "Alex Shin's Snow Fort")
-  (url "https://snow-fort.org/s/repo.scm")))
+  (url "https://snow-fort.org/s/repo.scm"))
+
+ (package
+   (version "0.0.1")
+   (library
+     (name
+       (sdl))
+     (path "sdl.sld")
+     (depends
+       (chibi)))
+   (description "SDL bindings")
+   (url "https://hwinslow3.github.io/domus-repo/packages/sdl-0.0.1.tgz"))
+)
