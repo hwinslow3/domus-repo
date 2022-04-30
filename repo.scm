be22d9f00ebac8a5553a2c761c97b56ca3fe1e45
Howard Winslow <395849+hwinslow3@users.noreply.github.com>
2022-04-29 20:30:06 -0400

    readded alex's repo

--- repo.scm
+++ repo.scm
@@ -1,12 +1,15 @@
 (repository
+ (sibling
+  (name "Alex Shin's Snow Fort")
+  (url "https://snow-fort.org/s/repo.scm"))
  (package
-   (version "0.0.1")
-   (library
-     (name
+  (version "0.0.1")
+  (library
+      (name
        (sdl))
-     (path "sdl.sld")
-     (depends
-       (chibi)))
-   (description "SDL bindings")
-   (url "https://hwinslow3.github.io/domus-repo/packages/sdl-0.0.1.tgz"))
+    (path "sdl.sld")
+    (depends
+     (chibi)))
+  (description "SDL bindings")
+  (url "https://hwinslow3.github.io/domus-repo/packages/sdl-0.0.1.tgz"))
 )
